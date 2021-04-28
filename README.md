683dd3c698b7400674affbbc7ba56008a4abeccc
Chetan Agarwal <chetan86866@gmail.com>
2021-04-28 14:29:19 +0530

    cheering sound when full

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # batteryfull
 
-A new Flutter project.
+A utility app for triggering alarm when phone's battery is full.
 
-## Getting Started
+## Screenshot
 
-This project is a starting point for a Flutter application.
+<div>
+    <img src="/screenshots/screenshot.png" width="400px"</img> 
+    <img src="/screenshots/ss.png" width="400px"</img> 
+</div>
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,24 @@
 import 'dart:async';
 
-import 'package:battery/battery.dart';
+import 'package:battery_plus/battery_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
+
+Soundpool _soundpool;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Battery Alert'),
@@ -33,15 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,51 +41,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   StreamSubscription<BatteryState> _batteryStateSubscription;
 
-  void startBatterySub() {
-    _batteryStateSubscription =
-        _battery.onBatteryStateChanged.listen((BatteryState state) {
-      if (state == BatteryState.full) {
-        FlutterRingtonePlayer.playAlarm();
-      } else if (state == BatteryState.charging) {
-        setState(() async {
-          _batteryPercent = await _battery.batteryLevel;
+  Future<int> _cheeringId;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSounds();
+  }
+
+  Future<void> startBatterySub() async {
+    if (_batteryStateSubscription == null) {
+      int batLevel = await _battery.batteryLevel;
+
+      _batteryStateSubscription =
+          _battery.onBatteryStateChanged.listen((BatteryState state) {
+        setState(() {
+          _batteryPercent = batLevel;
         });
-      }
-    });
+
+        if (state == BatteryState.full || batLevel == 100) {
+          _playCheering();
+        }
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -118,13 +92,28 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          startBatterySub();
-        },
+        onPressed: startBatterySub,
         tooltip: 'BatteryMonitor',
         child: Icon(Icons.alarm),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+
+  Future<void> _playCheering() async {
+    var _sound = await _cheeringId;
+    await _soundpool.play(
+      _sound,
+    );
+  }
+
+  Future<int> _loadCheering() async {
+    var asset = await rootBundle.load("sounds/c-c-1.mp3");
+    return await _soundpool.load(asset);
+  }
+
+  Future<void> _loadSounds() async {
+    _soundpool ??= Soundpool();
+    _cheeringId = _loadCheering();
   }
 
   @override
@@ -132,6 +121,9 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
     if (_batteryStateSubscription != null) {
       _batteryStateSubscription.cancel();
+    }
+    if (_soundpool != null) {
+      _soundpool.dispose();
     }
   }
 }
